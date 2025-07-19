dc544d80d57401cc2bd9d56ec5f42580c5b31cae
prd1252 <deshpr29@gmail.com>
2025-07-19 20:11:47 +0530

    initial commit

---
+++ backend/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+time_logs = []
+
+@app.route('/log-time', methods=['POST'])
+def log_time():
+    data = request.json
+    time_logs.append(data)
+    return jsonify({"message": "Time logged successfully"}), 201
+
+@app.route('/logs', methods=['GET'])
+def get_logs():
+    return jsonify(time_logs)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
